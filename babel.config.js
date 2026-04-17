88fa6af7c7f889b092f1fd245526b4ab24f8f1e3
jiachengwang-punch <jiachengwangjch@gmail.com>
2026-04-17 19:02:10 +0800

    fix: remove reanimated plugin, handled by babel-preset-expo

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
-  const isWeb = process.env.EXPO_PLATFORM === 'web';
   return {
     presets: ['babel-preset-expo'],
-    plugins: isWeb ? [] : ['react-native-reanimated/plugin'],
   };
 };
